731afbf57ab86064e08ce27bce6a5d16fab2f907
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2021-05-03 14:42:00 -0400

    Build in Place

--- Lifetime of Opportunity/Assets/_Scripts/EventMarker.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EventMarker : MonoBehaviour
-{
-    public int eventID;
-    public int eventYear;
-}
--- Lifetime of Opportunity/Assets/_Scripts/Year.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Year : MonoBehaviour
-{
-    public int firstEvent;
-    public int lastEvent;
-}
